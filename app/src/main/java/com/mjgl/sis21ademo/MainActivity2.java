a48c54e0bc6ce956e926c54ac5f85f9d64cb68a9
manchovi1 <winmanuel08@gmail.com>
2020-09-21 06:46:10 -0600

    Ventanas Emergentes(MODAL), toast custom  y menus FABToolbarLayout

---
+++ app/src/main/java/com/mjgl/sis21ademo/MainActivity2.java
@@ -0,0 +1,231 @@
+package com.mjgl.sis21ademo;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    modal_Toast_Custom customWindow = new modal_Toast_Custom();
+
+    private FABToolbarLayout morph;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Salir")
+                    .setMessage("¿Esta seguro?\n\nSe retornará al menú principal.")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                           finish();
+                        }
+                    })
+                    .show();
+            // Si el listener devuelve true, significa que el evento esta procesado, y nadie debe hacer nada mas
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+        /*FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        morph = findViewById(R.id.fabtoolbar);
+
+        View uno, dos, tres, cuatro, cinco;
+
+        uno = findViewById(R.id.uno);
+        dos = findViewById(R.id.dos);
+        cuatro = findViewById(R.id.cuatro);
+        tres = findViewById(R.id.tres);
+        cinco = findViewById(R.id.cinco);
+
+        fab.setOnClickListener(this);
+        uno.setOnClickListener(this);
+        dos.setOnClickListener(this);
+        tres.setOnClickListener(this);
+        cuatro.setOnClickListener(this);
+        cinco.setOnClickListener(this);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.callModalBasic(MainActivity2.this);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.modalConfirm(MainActivity2.this);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.dialogConfirm1(MainActivity2.this);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.dialogConfirm2(MainActivity2.this);
+            }
+        });
+
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.dialogConfirmCustom1(MainActivity2.this);
+            }
+        });
+
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.dialogConfirmCustom2(MainActivity2.this);
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.dialogAbout(MainActivity2.this);
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customWindow.dialogForm(MainActivity2.this);
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Registro Eliminado Correctamente.");
+            }
+        });
+    }
+
+
+
+    public void showToast(String message) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
+
+        TextView toastText = layout.findViewById(R.id.toast_text);
+        ImageView toastImage = layout.findViewById(R.id.toast_image);
+
+        toastText.setText(message);
+        toastImage.setImageResource(R.drawable.ic_delete);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+
+        toast.show();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab) {
+            morph.show();
+        }
+
+        //morph.hide();
+
+        switch (v.getId()){
+
+            case R.id.uno:
+                showToast("Clic boton 1");
+                customWindow.dialogConfirm4(MainActivity2.this);
+                morph.hide();
+                break;
+
+            case R.id.dos:
+                showToast("Clic boton 2");
+                morph.hide();
+                break;
+
+            case R.id.tres:
+                showToast("Clic boton 3");
+                morph.hide();
+                break;
+
+            case R.id.cuatro:
+                showToast("Clic boton 4");
+                morph.hide();
+                break;
+
+            case R.id.cinco:
+                //showToast("Cerrando menu");
+                morph.hide();
+                break;
+
+            default:
+                morph.hide();
+                break;
+
+        }
+
+    }
+}
